7a4d7987cd63ce272bb7c4278c78823eeb5efcb9
faizandevx <faizandevx@gmail.com>
2025-10-24 05:28:58 -0700

    Functions in PHP

--- index_02.php
+++ index_02.php
@@ -985,6 +985,88 @@ foreach ($colors as $x) {
   echo "$x <br>";
 }
 
+//PHP Functions
+echo "<br>";
+//Create a Function
+function myMessage() {
+  echo "Hello world!";
+}
+
+myMessage();
+
+//PHP Function Arguments
+// function familyName($fname) {
+//   echo "$fname Refsnes.<br>";
+// }
+
+// familyName("Jani");
+// familyName("Hege");
+// familyName("Stale");
+// familyName("Kai Jim");
+// familyName("Borge");
+
+function familyName($fname, $year) {
+  echo "$fname Refsnes. Born in $year <br>";
+}
+
+familyName("Faizan", "1975");
+familyName("Farhan", "1978");
+familyName("Waqas", "1983");
+
+//PHP Default Argument Value
+function setHeight($minheight = 50) {
+  echo "The height is : $minheight <br>";
+}
+
+setHeight(350);
+setHeight(); // will use the default value of 50
+setHeight(135);
+setHeight(80);
+
+function sum($x, $y) {
+  $z = $x + $y;
+  return $z;
+}
+
+//PHP Functions - Returning values
+echo "5 + 10 = " . sum(5,10) . "<br>";
+echo "7 + 13 = " . sum(7,13) . "<br>";
+echo "2 + 4 = " . sum(2,4);
+
+//Passing Arguments by Refrence
+function add_five(&$value) {
+  $value += 5;
+}
+
+$num = 2;
+add_five($num);
+echo $num;
+
+//Variable Number of Arguments
+function sumMyNumbers(...$x) {
+  $n = 0;
+  $len = count($x);
+  for($i = 0; $i < $len; $i++) {
+    $n += $x[$i];
+  }
+  return $n;
+}
+
+$a = sumMyNumbers(5, 2, 6, 2, 7, 7);
+echo $a;
+
+function myFamily($lastname, ...$firstname) {
+  $txt = "";
+  $len = count($firstname);
+  for($i = 0; $i < $len; $i++) {
+    $txt = $txt."Hi, $firstname[$i] $lastname.<br>";
+  }
+  return $txt;
+}
+
+$a = myFamily("Doe", "Jane", "John", "Joey");
+echo $a;
+
 ?>
 </body>
 </html>
