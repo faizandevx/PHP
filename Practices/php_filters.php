4755e56f5d9cdf9dea980e92f9b8dfcf47713b34
faizandevx <faizandevx@gmail.com>
2025-10-31 06:48:25 -0700

    PHP Exceptions

--- Practices/php_filters.php
+++ Practices/php_filters.php
@@ -203,6 +203,78 @@ foreach($arr as $key => $value) {
   echo $key . " => " . $value . "<br>";
 }
 
+//PHP Exceptions
+// function divide($dividend, $divisor) {
+//   if($divisor == 0) {
+//     throw new Exception("Division by zero");
+//   }
+  // return $dividend / $divisor;
+// }
+
+echo divide(5, 0);
+
+//The try...catch Statement
+// function divide($dividend, $divisor) {
+//   if($divisor == 0) {
+//     throw new Exception("Division by zero");
+//   }
+//   return $dividend / $divisor;
+// }
+
+try {
+  echo divide(5, 0);
+} catch(Exception $e) {
+  echo "Unable to divide.";
+}
+
+//The try...catch...finally Statement
+// function divide($dividend, $divisor) {
+//   if($divisor == 0) {
+//     throw new Exception("Division by zero");
+//   }
+//   return $dividend / $divisor;
+// }
+
+try {
+  echo divide(5, 0);
+} catch(Exception $e) {
+  echo "Unable to divide. ";
+} finally {
+  echo "Process complete.";
+}
+
+// function divide($dividend, $divisor) {
+//   if($divisor == 0) {
+//     throw new Exception("Division by zero");
+//   }
+//   return $dividend / $divisor;
+// }
+
+try {
+  echo divide(5, 0);
+} finally {
+  echo "Process complete.";
+}
+
+//The Exception Object
+function divide($dividend, $divisor) {
+  if($divisor == 0) {
+    throw new Exception("Division by zero", 1);
+  }
+  return $dividend / $divisor;
+}
+
+try {
+  echo divide(5, 0);
+} catch(Exception $ex) {
+  $code = $ex->getCode();
+  $message = $ex->getMessage();
+  $file = $ex->getFile();
+  $line = $ex->getLine();
+  echo "Exception thrown in $file on line $line: [Code $code]
+  $message";
+}
+
 ?>
 
 </body>
