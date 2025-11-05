9f1a3aa8743b11d72d950da04f843cb74149cbdb
Faizan Ali <faizandevx@gmail.com>
2025-11-05 20:49:56 +0500

    PHP OOP - Access Modifiers

--- Practices/index_04_oop.php
+++ Practices/index_04_oop.php
@@ -161,22 +161,47 @@ echo "<br>";
 //PHP OPP Destructor
 echo "<br>";
 //PHP-The __Destructor Function
-class Fruit {
+// class Fruit {
   // Properties
-  var $name;
-  var $color;
+//   var $name;
+//   var $color;
 
-  // Methods
-  function __construct($name, $color) {
-    $this->name = $name;
-    $this->color = $color; 
+//   // Methods
+//   function __construct($name, $color) {
+//     $this->name = $name;
+//     $this->color = $color; 
+//   }
+//   function __destruct() {
+//     echo "The fruit is {$this->name} and the color is {$this->color}."; 
+//   }
+// }
+
+$apple = new Fruit("Apple", "red");
+
+//PHP OOP - Access Modifiers
+echo "<br>";
+//PHP - Access Modifiers
+class Fruit {
+  public $name;
+  public $color;
+  public $weight;
+
+  function set_name($n) {  // a public function (default)
+    $this->name = $n;
   }
-  function __destruct() {
-    echo "The fruit is {$this->name} and the color is {$this->color}."; 
+  protected function set_color($n) { // a protected function
+    $this->color = $n;
+  }
+  private function set_weight($n) { // a private function
+    $this->weight = $n;
   }
 }
 
-$apple = new Fruit("Apple", "red");
+$mango = new Fruit();
+$mango->set_name('Mango'); // OK
+$mango->set_color('Yellow'); // ERROR
+$mango->set_weight('300'); // ERROR
+
 ?>
 </body>
 </html>
