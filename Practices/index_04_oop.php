064445907bdac4e062cc7cd8e0bb967b40ee64b9
Faizan Ali <faizandevx@gmail.com>
2025-11-06 16:54:32 +0500

    PHP Inheritance and the Types

--- Practices/index_04_oop.php
+++ Practices/index_04_oop.php
@@ -176,7 +176,7 @@ echo "<br>";
 //   }
 // }
 
-$apple = new Fruit("Apple", "red");
+// $apple = new Fruit("Apple", "red");
 
 //PHP OOP - Access Modifiers
 echo "<br>";
@@ -208,6 +208,75 @@ echo "<br>";
 //PHP OOP-Inheritance
 echo "<br>";
 //OOP Inheritance
+// class Fruit {
+//   public $name;
+//   public $color;
+//   public function __construct($name, $color) {
+//     $this->name = $name;
+//     $this->color = $color; 
+//   }
+//   public function intro() {
+//     echo "The fruit is {$this->name} and the color is {$this->color}."; 
+//   }
+// }
+
+// Strawberry is inherited from Fruit
+// class Strawberry extends Fruit {
+//   public function message() {
+//     echo "Am I a fruit or a berry? "; 
+//   }
+// }
+
+// $strawberry = new Strawberry("Strawberry", "red");
+// $strawberry->message();
+// $strawberry->intro();
+
+// class Fruit {
+//   public $name;
+//   public $color;
+//   public function __construct($name, $color) {
+//     $this->name = $name;
+//     $this->color = $color; 
+//   }
+//   protected function intro() {
+//     echo "The fruit is {$this->name} and the color is {$this->color}."; 
+//   }
+// }
+
+// class Strawberry extends Fruit {
+//   public function message() {
+//     echo "Am I a fruit or a berry? "; 
+//   }
+// }
+
+// Try to call all three methods from outside class
+// $strawberry = new Strawberry("Strawberry", "red");  OK. __construct() is public
+// $strawberry->message(); OK. message() is public
+// $strawberry->intro(); ERROR. intro() is protected
+
+// class Fruit {
+//   public $name;
+//   public $color;
+//   public function __construct($name, $color) {
+//     $this->name = $name;
+//     $this->color = $color; 
+//   }
+//   protected function intro() {
+//     echo "The fruit is {$this->name} and the color is {$this->color}."; 
+//   }
+// }
+
+// class Strawberry extends Fruit {
+//   public function message() {
+//     echo "Am I a fruit or a berry? ";
+    // Call protected function from within derived class - OK 
+    // $this -> intro();
+//   }
+// }
+// $strawberry = new Strawberry("Strawberry", "red");  OK. __construct() is public
+// $strawberry->message(); OK. message() is public and it calls intro() (which is protected) from within the derived class
+
+//PHP - Overriding Inherited Methods
 class Fruit {
   public $name;
   public $color;
@@ -220,15 +289,19 @@ class Fruit {
   }
 }
 
-// Strawberry is inherited from Fruit
 class Strawberry extends Fruit {
-  public function message() {
-    echo "Am I a fruit or a berry? "; 
+  public $weight;
+  public function __construct($name, $color, $weight) {
+    $this->name = $name;
+    $this->color = $color;
+    $this->weight = $weight; 
+  }
+  public function intro() {
+    echo "The fruit is {$this->name}, the color is {$this->color}, and the weight is {$this->weight} gram."; 
   }
 }
 
-$strawberry = new Strawberry("Strawberry", "red");
-$strawberry->message();
+$strawberry = new Strawberry("Strawberry", "red", 50);
 $strawberry->intro();
 
 ?>
