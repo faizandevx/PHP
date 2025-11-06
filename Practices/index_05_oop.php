96b1e5acf0064a2c8c72feeb9a658d811023b5f2
Faizan Ali <faizandevx@gmail.com>
2025-11-06 21:37:43 +0500

    PHP Statice Method

--- Practices/index_05_oop.php
+++ Practices/index_05_oop.php
@@ -218,6 +218,62 @@ $obj2 = new Welcome2();
 $obj2->msg1();
 $obj2->msg2();
 
+//PHP OOP - Static Methods
+echo "<br>";
+//PHP-Static Method
+// class greeting {
+//   public static function welcome() {
+//     echo "Hello World!";
+//   }
+//  }
+
+// Call static method
+greeting::welcome();
+
+//PHP - More on Static Methods
+echo "<br>";
+class greeting {
+  public static function welcome() {
+    echo "Hello World!";
+  }
+  public function __construct() {
+    self::welcome();
+  }
+}
+
+new greeting();
+
+class A {
+  public static function welcome() {
+    echo "Hello World!";
+  }
+}
+
+class B {
+  public function message() {
+    A::welcome();
+  }
+}
+
+$obj = new B();
+echo $obj -> message(); 
+
+class domain {
+  protected static function getWebsiteName() {
+    return "W3Schools.com";
+  }
+}
+
+class domainW3 extends domain {
+  public $websiteName;
+  public function __construct() {
+    $this->websiteName = parent::getWebsiteName();
+  }
+}
+
+$domainW3 = new domainW3;
+echo $domainW3 -> websiteName;
+
 ?>
 
 </body>
