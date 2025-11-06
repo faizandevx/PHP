cad64b00159e720a974dea387531451a0eff086e
Faizan Ali <faizandevx@gmail.com>
2025-11-06 19:54:31 +0500

    The Other Example for Abstract in PHP

--- Practices/index_05_oop.php
+++ Practices/index_05_oop.php
@@ -95,6 +95,31 @@ echo $class->prefixName("John Doe");
 echo "<br>"; 
 echo $class->prefixName("Jane Doe");
 
+echo "<br>";
+abstract class ParentClass {
+  // Abstract method with an argument
+  abstract protected function prefixName($name);
+}
+
+class ChildClass extends ParentClass {
+  // The child class may define optional arguments that is not in the parent's abstract method
+  public function prefixName($name, $separator = ".", $greet = "Dear") {
+    if ($name == "John Doe") {
+      $prefix = "Mr";
+    } elseif ($name == "Jane Doe") {
+      $prefix = "Mrs";
+    } else {
+      $prefix = "";
+    }
+    return "{$greet} {$prefix}{$separator} {$name}";
+  }
+}
+
+$class = new ChildClass;
+echo $class->prefixName("John Doe");
+echo "<br>";  
+echo $class->prefixName("Jane Doe");
+
 ?>
 
 </body>
