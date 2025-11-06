5267d46ee6719edf80fda108912abdef0042f306
Faizan Ali <faizandevx@gmail.com>
2025-11-06 19:51:21 +0500

    PHP Abstract Class

--- Practices/index_05_oop.php
+++ Practices/index_05_oop.php
@@ -12,12 +12,88 @@
 //PHP OOP - Class Constants
 echo "<br>";
 //PHP - Class Constants
+// class Goodbye {
+//   const LEAVING_MESSAGE = "Thank you for visiting W3Schools.com!";
+// }
+
+// echo Goodbye::LEAVING_MESSAGE;
+
 class Goodbye {
   const LEAVING_MESSAGE = "Thank you for visiting W3Schools.com!";
+  public function byebye() {
+    echo self::LEAVING_MESSAGE;
+  }
 }
 
-echo Goodbye::LEAVING_MESSAGE;
+$goodbye = new Goodbye();
+$goodbye->byebye();
 
+//PHP OOP - Abstract Classes
+echo "<br>";
+
+// Parent class
+abstract class Car {
+  public $name;
+  public function __construct($name) {
+    $this->name = $name;
+  }
+  abstract public function intro() : string;
+}
+
+// Child classes
+class Audi extends Car {
+  public function intro() : string {
+    return "Choose German quality! I'm an $this->name!";
+  }
+}
+
+class Volvo extends Car {
+  public function intro() : string {
+    return "Proud to be Swedish! I'm a $this->name!";
+  }
+}
+
+class Citroen extends Car {
+  public function intro() : string {
+    return "French extravagance! I'm a $this->name!";
+  }
+}
+
+// Create objects from the child classes
+$audi = new audi("Audi");
+echo $audi->intro();
+echo "<br>";
+
+$volvo = new volvo("Volvo");
+echo $volvo->intro();
+echo "<br>";
+
+$citroen = new citroen("Citroen");
+echo $citroen->intro();
+
+//PHP - More Abstract Class Examples
+echo "<br>";
+abstract class ParentClass {  // Abstract method with an argument
+  abstract protected function prefixName($name);
+}
+
+class ChildClass extends ParentClass {
+  public function prefixName($name) {
+    if ($name == "John Doe") {
+      $prefix = "Mr.";
+    } elseif ($name == "Jane Doe") {
+      $prefix = "Mrs.";
+    } else {
+      $prefix = "";
+    }
+    return "{$prefix} {$name}";
+  }
+}
+
+$class = new ChildClass;
+echo $class->prefixName("John Doe");
+echo "<br>"; 
+echo $class->prefixName("Jane Doe");
 
 ?>
 
