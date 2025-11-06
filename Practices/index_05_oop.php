c3af86be2c898c69bcda966d66c5461052697f58
Faizan Ali <faizandevx@gmail.com>
2025-11-06 21:23:59 +0500

    Traits in PHP

--- Practices/index_05_oop.php
+++ Practices/index_05_oop.php
@@ -73,27 +73,27 @@ echo $citroen->intro();
 
 //PHP - More Abstract Class Examples
 echo "<br>";
-abstract class ParentClass {  // Abstract method with an argument
-  abstract protected function prefixName($name);
-}
+// abstract class ParentClass {   Abstract method with an argument
+//   abstract protected function prefixName($name);
+// }
 
-class ChildClass extends ParentClass {
-  public function prefixName($name) {
-    if ($name == "John Doe") {
-      $prefix = "Mr.";
-    } elseif ($name == "Jane Doe") {
-      $prefix = "Mrs.";
-    } else {
-      $prefix = "";
-    }
-    return "{$prefix} {$name}";
-  }
-}
+// class ChildClass extends ParentClass {
+//   public function prefixName($name) {
+//     if ($name == "John Doe") {
+//       $prefix = "Mr.";
+//     } elseif ($name == "Jane Doe") {
+//       $prefix = "Mrs.";
+//     } else {
+//       $prefix = "";
+//     }
+//     return "{$prefix} {$name}";
+//   }
+// }
 
-$class = new ChildClass;
-echo $class->prefixName("John Doe");
-echo "<br>"; 
-echo $class->prefixName("Jane Doe");
+// $class = new ChildClass;
+// echo $class->prefixName("John Doe");
+// echo "<br>"; 
+// echo $class->prefixName("Jane Doe");
 
 echo "<br>";
 abstract class ParentClass {
@@ -119,6 +119,104 @@ $class = new ChildClass;
 echo $class->prefixName("John Doe");
 echo "<br>";  
 echo $class->prefixName("Jane Doe");
+
+//PHP OOP - Interfaces
+echo "<br>";
+//PHP-Using Interfaces
+// interface Animal {
+//   public function makeSound();
+// }
+
+// class Cat implements Animal {
+//   public function makeSound() {
+//     echo "Meow";
+//   }
+// }
+
+// $animal = new Cat();
+// $animal->makeSound();
+
+//The Other Example
+// Interface definition
+interface Animal {
+  public function makeSound();
+}
+
+// Class definitions
+class Cat implements Animal {
+  public function makeSound() {
+    echo " Meow ";
+  }
+}
+
+class Dog implements Animal {
+  public function makeSound() {
+    echo " Bark ";
+  }
+}
+
+class Mouse implements Animal {
+  public function makeSound() {
+    echo " Squeak ";
+  }
+}
+
+// Create a list of animals
+$cat = new Cat();
+$dog = new Dog();
+$mouse = new Mouse();
+$animals = array($cat, $dog, $mouse);
+
+// Tell the animals to make a sound
+foreach($animals as $animal) {
+  $animal->makeSound();
+} 
+
+//PHP OOP - Traits
+echo "<br>";
+
+// trait message1 {
+// public function msg1() {
+//     echo "OOP is fun! ";
+//   }
+// }
+
+// class Welcome {
+//   use message1;
+// }
+
+// $obj = new Welcome();
+// $obj->msg1();
+
+//PHP - Using Multiple Traits
+trait message1 {
+  public function msg1() {
+    echo "OOP is fun! "; 
+  }
+}
+
+trait message2 {
+  public function msg2() {
+    echo "OOP reduces code duplication!"; 
+  }
+}
+
+class Welcome {
+  use message1;
+}
+
+class Welcome2 {
+  use message1, message2;
+}
+
+$obj = new Welcome();
+$obj->msg1();
+echo "<br>";
+
+
+$obj2 = new Welcome2();
+$obj2->msg1();
+$obj2->msg2();
 
 ?>
 
