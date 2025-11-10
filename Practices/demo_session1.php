b16adc2c3c969590e92f4d04ecaa459ac18527ee
Faizan Ali <faizandevx@gmail.com>
2025-11-10 17:00:15 +0500

    Re Practicing the Sessions in PHP

--- Practices/demo_session1.php
+++ Practices/demo_session1.php
@@ -11,10 +11,66 @@ session_start();
 $_SESSION["favcolor"] = "green";
 $_SESSION["favanimal"] = "cat";
 echo "Session variables are set.";
+echo "<br>";
 ?>
 
 <!-- Get PHP Session Variable Values -->
  
+
+</body>
+</html>
+
+
+<?php
+// Start the session
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// Set session variables
+$_SESSION["favcolor"] = "green";
+$_SESSION["favanimal"] = "cat";
+echo "Session variables are set.";
+echo "<br>";
+?>
+
+<!-- Get PHP Session Variable Values -->
+ 
+
+</body>
+</html>
+
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// Echo session variables that were set on previous page
+echo "Favorite color is " . $_SESSION["favcolor"] . ".<br>";
+echo "Favorite animal is " . $_SESSION["favanimal"] . ".";
+echo "<br>";
+?>
+
+</body>
+</html>
+
+
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+print_r($_SESSION);
+?>
 
 </body>
 </html>
--- Practices/index_05_oop.php
+++ Practices/index_05_oop.php
@@ -332,47 +332,47 @@ echo $domainW3 -> websiteName;
 //PHP - Creating Iterables
 echo "<br>";
 // Create an Iterator
-class MyIterator implements Iterator {
-  private $items = [];
-  private $pointer = 0;
+// class MyIterator implements Iterator {
+//   private $items = [];
+//   private $pointer = 0;
 
-  public function __construct($items) {
+//   public function __construct($items) {
     // array_values() makes sure that the keys are numbers
-    $this->items = array_values($items);
-  }
+  //   $this->items = array_values($items);
+  // }
 
-  public function current() {
-    return $this->items[$this->pointer];
-  }
+  // public function current() {
+  //   return $this->items[$this->pointer];
+  // }
 
-  public function key() {
-    return $this->pointer;
-  }
+  // public function key() {
+  //   return $this->pointer;
+  // }
 
-  public function next() {
-    $this->pointer++;
-  }
+  // public function next() {
+  //   $this->pointer++;
+  // }
 
-  public function rewind() {
-    $this->pointer = 0;
-  }
+  // public function rewind() {
+  //   $this->pointer = 0;
+  // }
 
-  public function valid() {
+  // public function valid() {
     // count() indicates how many items are in the list
-    return $this->pointer < count($this->items);
-  }
-}
+//     return $this->pointer < count($this->items);
+//   }
+// }
 
 // A function that uses iterables
-function printIterable(iterable $myIterable) {
-  foreach($myIterable as $item) {
-    echo $item;
-  }
-}
+// function printIterable(iterable $myIterable) {
+//   foreach($myIterable as $item) {
+//     echo $item;
+//   }
+// }
 
 // Use the iterator as an iterable
-$iterator = new MyIterator(["a", "b", "c"]);
-printIterable($iterator);
+// $iterator = new MyIterator(["a", "b", "c"]);
+// printIterable($iterator);
 
 ?>
 
